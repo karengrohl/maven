900ad8b768fc4bf9613e1fd719a3b486f10d4a6d
Anastasia Churilina <samwhibley21@gmail.com>
2017-12-09 01:17:32 +0300

    Initial commit

---
+++ src/org/bsuir/gr573901/Churilina/laba6/proj1/Main.java
@@ -0,0 +1,14 @@
+
+package org.bsuir.gr573901.Churilina.laba6.proj1;
+import org.bsuir.gr573901.Churilina.laba6.proj1.util.PrintStr;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) 
+    {
+        PrintStr.Print(args);     
+    }
+    
+}
---
+++ src/org/bsuir/gr573901/Churilina/laba6/proj1/util/PrintStr.java
@@ -0,0 +1,13 @@
+
+package org.bsuir.gr573901.Churilina.laba6.proj1.util;
+
+
+public class PrintStr
+{
+    static public void Print(String []strings){
+	System.out.println("Entered text:");
+        for(int i = 0; i < strings.length; i++){
+            System.out.println(strings[i]);
+        }
+    }
+}
